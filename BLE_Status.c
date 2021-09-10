bb122657c7a4d7575d0c82028a37c81218b57ee4
Firas Faham <59860028+orcad@users.noreply.github.com>
2021-09-09 21:35:40 -0400

    BLE Connection Status verification using C code
    The Server BLE module passes the status to the MCU and the embedded C code checks the status. The status message is then reflected in the BLE Server.

---
+++ BLE_Status.c
@@ -0,0 +1,60 @@
+
+/**
+ * \ BLE Interface
+ * \ Status Message
+ *
+ * This API is used to handle incoming status messages.
+ * It prints all status messages, If DISCONNECT or STREAM_OPEN is received it manages
+ * the state of bool connected.
+ *
+ * \param message Passed status message
+ * \return Nothing
+ */
+      // File: BLE_Interface.c
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *BLE_msg = "DISCONNECT";
+char connected = 'N';
+char *message ;
+
+char BLE_MessageHandler(char* );
+
+int main()
+{
+    printf("Check BLE connection\n\n");
+
+        message = "DISCONNECT";       // Will print > Not connected
+//      message = "STREAM_OPEN";      // Will print > Connected
+
+    connected = BLE_MessageHandler(message);
+
+     if (connected == 'Y') printf("Connected...\n");
+     if (connected == 'N') printf("Not Connected...\n");
+
+    return 0;
+}
+
+
+
+
+
+char BLE_MessageHandler(char* message)
+{
+
+    if (strstr(message, "DISCONNECT"))
+    {
+        return (char)('N');
+    }
+    else if (strstr(message, "STREAM_OPEN"))
+    {
+        return (char)('Y');
+    }
+    else
+    {
+        // Place your message here, such as for ERROR
+    }
+}
